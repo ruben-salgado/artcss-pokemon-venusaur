51aeadd3b4020eaf112c17c218aa1a153e11ce3f
Ruben Salgado <ing.ruben.salgado.ramirez@gmail.com>
2020-08-29 21:26:21 -0500

    Estructura base del proyecto y arbol de carpetas

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# artcss-pokemon-venusaur
-🐺 Dibujo con CSS (venusaur)
+# Dibujo Pokemon (Venusaur) con CSS
+
+<span style="display:block;text-align:center">[![version](https://svgshare.com/i/P9C.svg)](https://www.npmjs.com/package/npm)
+[![language](https://svgshare.com/i/PAE.svg)](https://www.npmjs.com/package/pug)
+[![language](https://svgshare.com/i/P97.svg)](https://www.npmjs.com/package/sass)
+[![license](https://svgshare.com/i/PAR.svg)](LICENSE)
+</span>
+
+Dibujo creado a partir de formas b&aacute;sicas, agregando un poco de complejidad con la propiedad clip-path.
+
+<details>
+  <summary>👁 Previsualizar</summary>
+  <p align="center">
+    <img width="350" height="300" src="src/images/preview.png">
+  </p>
+</details>
+
+---
+
+## 💻 Instalaci&oacute;n
+
+Verificar que se tiene instalado [Node.js](https://nodejs.org/es/) superior a 12.
+
+```bash
+  $ node --version
+  v12.18.3
+  $ npm --version
+  6.14.6
+```
+
+Instalaci&oacute;n de los m&oacute;dulos de node, dentro de la carpeta del proyecto.
+
+```bash
+  $ npm install
+```
+
+---
+
+## 🔁 Iniciar servidor de desarrollo con recarga autom&aacute;tica
+
+Para iniciar el servidor es necesario correr la instrucci&oacute;n
+
+```bash
+  $ npm run buildserver
+```
+
+> El servidor se ejecuta en el puerto 9000 y abre autom&aacute;ticamente una ventana del navegador (chrome) para visualizarlo.
+
+### 🔗 Demostraci&oacute;n
+
+Privsualizaci&oacute;n del c&oacute;digo en: [CodePen](https://t.ly/NLEc)
+
+---
+
+## 📝 Licencia
+
+[MIT](LICENSE)
+
---
+++ webpack.config.js
@@ -0,0 +1,149 @@
+const path                      = require('path');
+
+const webpack                   = require('webpack');
+const HtmlWebpackPlugin         = require('html-webpack-plugin');
+const UglifyJsPlugin            = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin   = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin }    = require('clean-webpack-plugin');
+const MiniCssExtractPlugin      = require('mini-css-extract-plugin');
+
+const title                    = 'Pokemon CSS';
+const publicPath               = '/pokemon/venusaur/';
+const portServer               = 9000;
+
+module.exports = function (env) {
+    var isProd = false;
+
+    if (env != null && env.production) {
+        isProd = true;
+    }
+
+    var configUseCssDev = ["style-loader", "css-loader", 'sass-loader'];
+    var configUseCssProd = [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+                hmr: process.env.NODE_ENV === 'development',
+                reloadAll: true,
+            },
+        },
+        {
+            loader: "css-loader",
+            options: {
+                sourceMap: true
+            }
+        },
+        "sass-loader"
+    ];
+
+    var configUseCss = isProd ? configUseCssProd : configUseCssDev;
+
+    return {
+        context: path.resolve(__dirname, "src"),
+        entry: {
+            app: './app.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: './[name].[hash].js'
+        },
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            publicPath: publicPath,
+            open: {
+                app: ['chrome', '--incognito', '--other-flag']
+            },
+            openPage: publicPath,
+            port: portServer
+        },
+        optimization: {
+            splitChunks: {
+                cacheGroups: {
+                    styles: {
+                        name: 'styles',
+                        test: /\.css$/,
+                        chunks: 'all',
+                        enforce: true,
+                    },
+                },
+            },
+            minimizer: [
+                new UglifyJsPlugin({
+                    cache: true,
+                    parallel: true,
+                    sourceMap: false
+                })
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin({
+                cleanAfterEveryBuildPatterns: ['dist']
+            }),
+            new HtmlWebpackPlugin({
+                template: './template/index.pug',
+                title: title,
+                filename: 'index.html',
+                minify: true,
+                hash: true
+            }),
+            new MiniCssExtractPlugin({
+                filename: isProd ? '[name].[hash:8].css' : '[name].css',
+                chunkFilename: isProd ? '[id].[hash:8].css' : '[id].css',
+            }),
+            new OptimizeCssAssetsPlugin({
+                assetNameRegExp: /\.css$/g,
+                cssProcessor: require('cssnano'),
+                cssProcessorPluginOptions: {
+                    preset: ['default', {
+                        discardComments: {
+                            removeAll: true
+                        }
+                    }],
+                },
+                canPrint: true
+            }),
+            new webpack.HotModuleReplacementPlugin(),
+            new webpack.NamedModulesPlugin()
+        ],
+        module: {
+            rules: [{
+                    test: /\.pug$/i,
+                    use: ['pug-loader']
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/i,
+                    use: configUseCss
+                },
+                {
+                    test: /\.(gif|png|jpe?g|svg)$/i,
+                    use: [
+                        'file-loader',
+                        {
+                            loader: 'image-webpack-loader',
+                            options: {
+                                mozjpeg: {
+                                    progressive: true,
+                                    quality: 65
+                                },
+                                optipng: {
+                                    enabled: false,
+                                },
+                                pngquant: {
+                                    quality: [0.50, 0.80],
+                                    speed: 10,
+                                    strip: true
+                                },
+                                gifsicle: {
+                                    interlaced: false,
+                                },
+                                webp: {
+                                    quality: 50
+                                }
+                            }
+                        },
+                    ],
+                },
+            ],
+        }
+    }
+}
